4908870b7d8ed9e78a6cf2eee8500334ab094252
Richard Wolf <richwolf817@gmail.com>
2019-05-30 18:11:34 -0600

    config.js

--- src/config.js
+++ src/config.js
@@ -2,17 +2,17 @@ const dev = {
   STRIPE_KEY: "pk_test_v1amvR35uoCNduJfkqGB8RLD",
   s3: {
     REGION: "us-east-1",
-    BUCKET: "notes-app-2-api-dev-attachmentsbucket-6wbhcogxihbo"
+    BUCKET: "notes-app-2-api-dev-attachmentsbucket-6bgefdqha4ox"
   },
   apiGateway: {
     REGION: "us-east-1",
-    URL: "https://api.serverless-stack.seed-demo.club/dev"
+    URL: "https://1y3xz4r22e.execute-api.us-east-1.amazonaws.com/dev"
   },
   cognito: {
     REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_yKOQs2dWK",
-    APP_CLIENT_ID: "1h0r8s763vnqj4id6dvguanp8f",
-    IDENTITY_POOL_ID: "us-east-1:dedfd34a-9d7e-4bf2-a1dd-ef603bac2ecb"
+    USER_POOL_ID: "us-east-1_Rmjrf5w7e",
+    APP_CLIENT_ID: "4otg89ovahd52uc562nkat6stj",
+    IDENTITY_POOL_ID: "us-east-1:963c1461-3188-4e18-bc82-047070b27f94"
   }
 };
 
